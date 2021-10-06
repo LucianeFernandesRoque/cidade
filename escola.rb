a8b3792279a0dd0b59983217450e089d8bf188d1
luciane <lucianefernandescdf@gmail.com>
2021-10-06 09:16:42 -0300

    add methods

--- escola.rb
+++ escola.rb
@@ -6,4 +6,12 @@ class Escola
     @professores = []
     @salas_de_aula = []
   end
+
+  def add_professor(professor)
+    @professores << professor
+  end
+  
+  def add_alunos(aluno)
+    @alunos << aluno
+  end
 end
