411d4345ebc845bf1f9ae93e8729353dcc88dbcd
luciane <lucianefernandescdf@gmail.com>
2021-10-08 08:54:32 -0300

    add teste

--- spec/escola_spec.rb
+++ spec/escola_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 require_relative '../escola'
+require_relative '../person'
 
 describe Escola do
 let(:escola) { Escola.new }
-let(:professor) {Escola.add_professor('Luciane','Roque',33)}
+
   it 'deve ter alunos' do
     expect(escola.alunos.size).to eq 0
   end
@@ -16,8 +17,16 @@ let(:professor) {Escola.add_professor('Luciane','Roque',33)}
     expect(escola.salas_de_aula.size).to eq 0
   end
 
-  it 'dete ter um metodo que add professores' do
-    professor
-    expect(escola.add_professor).to e
+  
+  it 'deve add professores' do
+    professor = Person.new('Paulo','Rodrigues', 30)
+    escola.add_professor(professor)
+    expect(escola.professores.size).to eq 1
+  end
+
+  it 'deve add alunos' do
+    aluno = Person.new('Luciane','Roque', 34)
+    escola.add_alunos(aluno)
+    expect(escola.alunos.size).to eq 1
   end
 end
