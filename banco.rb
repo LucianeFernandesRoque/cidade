1b6cf1832e1facd4312d99fd5b8c0fa9325ad9d4
luciane <lucianefernandescdf@gmail.com>
2021-10-13 21:43:23 -0300

    create class banco

--- banco.rb
+++ banco.rb
@@ -3,7 +3,7 @@ class Banco
   def initialize
     @quantidade_de_lugares = [0,1,2]
     @lugares_ocupados = [0,1,2]
-    @pessoas_sentadas = Person.new
+    @pessoas_sentadas = Person.new(sentar_no_banco)
   end
 
 end
