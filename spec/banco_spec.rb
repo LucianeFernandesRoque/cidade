3193b3a7eb91e42b1455269a3b2e6038238fe0a3
luciane <lucianefernandescdf@gmail.com>
2021-10-14 09:26:19 -0300

    modelagem de classe banco

--- spec/banco_spec.rb
+++ spec/banco_spec.rb
@@ -1,26 +1,37 @@
 require_relative '../banco'
- 
+require_relative '../person'
+
 describe Banco do
   let(:banco) { Banco.new }
   it 'deve ter 3 lugares do banco' do
-    expect(banco.quantidade_de_lugares.size).to eq 3
+    expect(banco.lugares_disponiveis).to eq 3
   end
-
-  it 'deve ter pessoas sentadas no banco' do
-    pessoas_sentadas = Person.new.sentar_no_banco
-    
+#aa(setup)
+#act
+#assert
+context 'ocupar banco' do
+  it 'deve ter uma pessoa sentada no banco' do
+      banco.ocupar_acento#executar
+    expect(banco.lugares_disponiveis).to eq 2
   end
+  
+  it 'deve ter duas pessoa sentada no banco' do
+    banco.ocupar_acento
+    banco.ocupar_acento
+    expect(banco.lugares_disponiveis).to eq 1
+  end
+end
 
   it 'o banco pode estar cheio' do
-    
+    banco.ocupar_acento
+    banco.ocupar_acento
+    banco.ocupar_acento
+    expect(banco.lugares_disponiveis).to eq 0    
   end
 
-  it 'o banco pode estar vazio' do
-    
-  end
-
-  it 'o banco pode ter um lugar disponivel' do
-    
+  it 'deve mostrar a quantidade de lugares disponiveis' do
+    banco.ocupar_acento
+    expect(banco.lugares_disponiveis).to eq 2
   end
 
   it 'o banco pode ter dois lugares disponiveis' do
