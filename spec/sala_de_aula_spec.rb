ab400cd0258323c63ad3a1a79b6b9d7956e7b53d
luciane <lucianefernandescdf@gmail.com>
2021-10-14 18:08:52 -0300

    teste sala de aula

---
+++ spec/sala_de_aula_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../sala_de_aula'
+describe SalaDeAula do
+  it 'deve ter carteiras' do
+    
+  end
+
+  it 'deve ter cadeiras' do
+    
+  end
+
+  it 'deve ter uma lousa' do
+    
+  end
+  it 'deve ter giz' do
+    
+  end
+  it 'deve ter alunos' do
+    
+  end
+
+  it 'deve ter professor' do
+    
+  end
+end
