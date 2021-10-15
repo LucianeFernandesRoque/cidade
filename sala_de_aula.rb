008d6fbac2c7e8bccdbbe5ba25ee08dce1ee6019
luciane <lucianefernandescdf@gmail.com>
2021-10-15 17:16:13 -0300

    add method lousa

--- sala_de_aula.rb
+++ sala_de_aula.rb
@@ -1,10 +1,11 @@
 class SalaDeAula
-  attr_reader :carteiras
-  def initialize(carteiras:)
+  attr_reader :carteiras, :lousa
+  def initialize(carteiras:,lousa:)
     @carteiras = carteiras
+    @lousa = lousa
   end
   
   def ocupar_carteira
-    @carteiras -= 1  
+    carteiras  << carteiras_ocupadas
   end
 end
--- spec/sala_de_aula_spec.rb
+++ spec/sala_de_aula_spec.rb
@@ -1,29 +1,32 @@
 require_relative '../sala_de_aula'
 describe SalaDeAula do
   it 'deve ter 20 carteiras' do
-    sala_de_aula = SalaDeAula.new(carteiras: 20)
+    sala_de_aula = SalaDeAula.new(carteiras: 20 ,lousa:'lousa')
     expect(sala_de_aula.carteiras).to eq 20
   end
 
   it 'deve ter 40 carteiras' do
-    sala_de_aula = SalaDeAula.new(carteiras: 40)
+    sala_de_aula = SalaDeAula.new(carteiras: 40, lousa:'lousa')
     expect(sala_de_aula.carteiras).to eq 40
   end
 
   it 'deve ter 10 carteiras ocupadas' do
-    sala_de_aula = SalaDeAula.new(carteiras: 3)
-    sala_de_aula.ocupar_carteira
-    expect(sala_de_aula.carteiras_ocupadas).to eq 1 
+    sala_de_aula = SalaDeAula.new(carteiras: 3, lousa:'lousa')
+    sala_de_aula.ocupar_carteira(carteiras: 2)
+    expect(sala_de_aula.carteiras_ocupadas).to eq 2
   end
 
   it 'deve ter 2 carteiras desocupadas' do
-    sala_de_aula = SalaDeAula.new(carteiras: 3)
+    sala_de_aula = SalaDeAula.new(carteiras: 3, lousa:'lousa')
     sala_de_aula.ocupar_carteira
     expect(sala_de_aula.carteiras_desocupadas).to eq 2 
   end
 
 
   it 'deve ter uma lousa' do
+    lousa = SalaDeAula.new('lousa') do
+      expect(sala_de_aula).to eq 'lousa'
+    end
     
   end
   it 'deve ter giz' do
