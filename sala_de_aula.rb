7976b383c45334d8139e22541127e43090364214
luciane <lucianefernandescdf@gmail.com>
2021-10-17 18:39:26 -0300

    rewrite test sala_de_aula

--- sala_de_aula.rb
+++ sala_de_aula.rb
@@ -1,12 +1,12 @@
 class SalaDeAula
-  attr_reader :carteiras, :lousa
+  attr_reader :carteiras, :lousa, :carteiras_ocupadas
   def initialize(carteiras: 20)
     @carteiras = carteiras
     @lousa = lousa
   end
   
   def ocupar_carteira
-    @carteiras  -=  carteiras_ocupadas
+    @carteiras  -= 1
   end
 
   def escrever_na_lousa(giz)
--- spec/sala_de_aula_spec.rb
+++ spec/sala_de_aula_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../sala_de_aula'
 describe SalaDeAula do
+  let(:sala_de_aula) {SalaDeAula.new('lousa','giz')}
   it 'deve ter 20 carteiras' do
     sala_de_aula = SalaDeAula.new(carteiras: 20)
     expect(sala_de_aula.carteiras).to eq 20
@@ -12,26 +13,19 @@ describe SalaDeAula do
 
   it 'deve ter 10 carteiras ocupadas' do
     sala_de_aula = SalaDeAula.new
-    sala_de_aula.ocupar_carteira(carteiras: 10)
-    expect(sala_de_aula.carteiras_ocupadas).to eq 2
+    pessoa = sala_de_aula.ocupar_carteira 
+    expect(sala_de_aula.ocupar_carteira).to eq 18
   end
-
-  it 'deve ter 2 carteiras desocupadas' do
-    sala_de_aula = SalaDeAula.new(carteiras: 3, lousa:'lousa')
-    sala_de_aula.ocupar_carteira
-    expect(sala_de_aula.carteiras_desocupadas).to eq 2 
-  end
-
 
   it 'deve ter uma lousa' do
-    lousa = SalaDeAula.new('lousa') do
+    lousa = SalaDeAula.new do
       expect(sala_de_aula).to eq 'lousa'
     end
     
   end
   it 'deve ter giz' do
-    giz = SalaDeAula.new('giz') do 
-      expect(sala_de_aula)
+    giz = SalaDeAula.new do 
+      expect(sala_de_aula).to eq 'giz'
     end
     
   end
