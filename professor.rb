7748e92dbcd50cdb7472ded376dedfffdd9a7453
luciane <lucianefernandescdf@gmail.com>
2021-10-22 21:15:54 -0300

    add professor heranca

--- professor.rb
+++ professor.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 class Professor < Person
   attr_reader :disciplina
-  attr_reader :id,:nome,:sobrenome,:idade,
-  def initialize(nome:,sobrenome:,idade:,id:)
+  attr_reader :id,:nome,:sobrenome,:idade
+  def initialize(nome,sobrenome,idade,id)
     super(nome,sobrenome,idade,id)
     @disciplina = []
   end
--- spec/professor_spec.rb
+++ spec/professor_spec.rb
@@ -1,23 +1,24 @@
 require_relative '../professor'
-
+require_relative '../person'
 
 describe Professor do
-  let(:professor) {Professor.new('Flouki', 'Flouki',33,10)}
+  let(:professor) {Professor.new('Flouki','Floukoso',33,100)}
 
-
-  it 'professor deve ter um id' do
-    expect(professor).to eq 8
-  end
 
     it 'professor deve ter uma disciplina' do
+      disciplina =  professor.disciplina
     expect(professor.disciplina.size).to eq 0
   end
 
   it 'professor deve ter um nome' do
-    expect(professor.nome).to eq'Flouki'
+    expect(professor.nome).to eq 'Flouki'
+  end
+
+  it 'professor deve ter uma idade' do
+    expect(professor.idade).to eq 33
   end
 
   it 'professor deve ter um sobrenome' do
-    expect(professor.sobrenome).to eq sobrenome
+    expect(professor.sobrenome).to eq 'Floukoso'
   end
 end
